5edf6b01f822ede0c8d4722ebed0c4fada47eefd
0Simba <b.dersoir@student.isartdigital.com>
2015-01-25 11:58:36 +0100

    Add flower entities self managing to add and controls destroying

--- src/entities/Flower.hx
+++ src/entities/Flower.hx
@@ -7,6 +7,7 @@ class Flower
 {
     static private var stateList:Array<String>;
     static private var config:Dynamic;
+
     private var referent:Events;
     private var stateIndex:Int;
 
@@ -17,19 +18,65 @@ class Flower
         stateIndex = _state;
 
         referent.emit("state changed", stateList[stateIndex]);
-        haxe.Timer.delay(endDelay, config.time.delay);
+        lunchDelay(grow, config.time.delay);
+
+        Flower.add(this);
     }
 
 
-    public function endDelay () {
+    public function grow () {
         stateIndex++;
         referent.emit("state changed", stateList[stateIndex]);
         if (stateList.length - 1 > stateIndex) {
-            haxe.Timer.delay(endDelay, config.time.delay);
+            lunchDelay(grow, config.time.delay);
         }
     }
 
     public function destroy () {
         referent.emit('destroying', null);
+        if (waitingCallback == 0) {
+            Flower.remove(this);
+        }
+        else {
+            toKill = true;
+        }
+    }
+
+
+
+
+    /***** YOU DON'T CARE *****/
+
+    private var waitingCallback:Int = 0;
+    private var toKill:Bool         = false;
+
+
+    private function lunchDelay (callback, time) {
+        waitingCallback++;
+        haxe.Timer.delay(function () {
+            endDelay(callback);
+        }, time);
+    }
+
+    private function endDelay (callback) {
+        waitingCallback--;
+        if (waitingCallback == 0 && toKill == true) {
+            Flower.remove(this);
+        }
+        else {
+            callback();
+        }
+    }
+
+
+        //List to manage destroying
+
+    static private var list:Array<Flower> = new Array<Flower>();
+    static private function add (flower:Flower) {
+        list.push(flower);
+    }
+    static private function remove (flower:Flower) {
+        var i = list.indexOf(flower);
+        list.splice(i, 1);
     }
 }
