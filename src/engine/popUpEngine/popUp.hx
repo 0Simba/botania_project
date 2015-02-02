d0b473f86b84e90bfea7da468ac20560f3958c52
0Simba <b.dersoir@student.isartdigital.com>
2015-02-02 22:10:06 +0100

    Add basic pop up compositing

--- src/engine/popUpEngine/popUp.hx
+++ src/engine/popUpEngine/popUp.hx
@@ -12,18 +12,36 @@ class PopUp
     public var name:String;
     public var container:Graphics;
 
+    public var pxSize:Vector2;
+    public var pxPos:Vector2;
+
     private var isoEngine:IsoEngine;
 
-    public function new (_name:String, size:Vector2, pos:Vector2) {
+    public function new (_name:String, pos:Vector2, size:Vector2) {
         isoEngine = IsoEngine.getInstance();
         name      = _name;
         container = isoEngine.displaying.createChildLayer(name, "popUp");
+
+        pxSize = new Vector2(size.x * isoEngine.width, size.y * isoEngine.height);
+        pxPos  = new Vector2(pos.x  * isoEngine.width, pos.y  * isoEngine.height);
+
+        container.beginFill(255255255, 0.7);
+        container.drawRect(pxPos.x, pxPos.y, pxSize.x, pxSize.y);
+        container.endFill();
     }
 
-    public function addBloc (size:Vector2, pos:Vector2, textureName:String) {
+    public function addBloc (pos:Vector2, size:Vector2, textureName:String) {
         var newBloc = new Hud();
         // newBloc.set( percentSize : utils.Vector2 , percentPos : utils.Vector2 , animationName : String , ?textureName : String , ?parentLayer : String )
-        // newBloc.set(size, pos, )
+
+        if (size.x <= 1 && size.y <= 1 && pos.x <= 1 && pos.y <= 1) {
+            size.x *= pxSize.x;
+            size.y *= pxSize.y;
+            pos.x  = (pos.x * pxSize.x) + pxPos.x;
+            pos.y  = (pos.y * pxSize.y) + pxPos.y;
+        }
+
+        newBloc.set(size, pos, textureName, name);
     }
 
     public function addYesNo () {
--- src/init/PopUp.hx
+++ src/init/PopUp.hx
@@ -8,9 +8,10 @@ class PopUp
 
     static public function load () {
         var popUpEngine = PopUpEngineMain.getInstance();
-        var welcome     = popUpEngine.create("welcome" , new Vector2(0.8, 0.8) , new Vector2(0.1, 0.1));
+        var welcome     = popUpEngine.create("welcome", new Vector2(0.1, 0.1), new Vector2(0.8, 0.8));
 
-        // welcome.addBloc();
+        welcome.addBloc(new Vector2(0.1, 0.1), new Vector2(0.8, 0.8), "savana");
+        welcome.addBloc(new Vector2(0.7, 0.7), new Vector2(0.2, 0.2), "automn");
     }
 
 }
