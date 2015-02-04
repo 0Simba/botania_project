5a38fe5facfe775760013f43c2c0d414d7c23992
0Simba <b.dersoir@student.isartdigital.com>
2015-02-04 19:25:34 +0100

    Spacing and comment

--- src/init/PopUp.hx
+++ src/init/PopUp.hx
@@ -8,8 +8,12 @@ class PopUp
 
     static public function load () {
         var popUpEngine = PopUpEngineMain.getInstance();
+
+            /***** Create Pattern *****/
         popUpEngine.createBlocPattern("background", Vector2.full, Vector2.zero, "pickBasic");
         popUpEngine.createButtonPattern("close", new Vector2(0.08, 0.08), new Vector2(0.92, 0), "digBasic", "digHover", "digClick", function(){});
+
+            /***** Create PopUp *****/
         init.popUps.BreakerPopUpInit.init(popUpEngine);
         init.popUps.FlowerPopUpInit.init(popUpEngine);
     }
