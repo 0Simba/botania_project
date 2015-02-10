d2c7c89563965bf0b62cc02a8199ea3cb55efdc7
Clement <c.garnier@student.isartdigital.com>
2015-02-11 00:46:55 +0100

    Maj fruit.     Ajout: Fonction Open & etablishNbSeed

--- src/entities/fruit/Fruit.hx
+++ src/entities/fruit/Fruit.hx
@@ -1,16 +1,31 @@
 package entities;
 
-//import engine.events.Events;
 import GameObject;
-
 import init.Config;
 import entities.genetic.Adn;
+import entities.Seed;
 
 class Fruit extends GameObject
 {
+	static public list:Array<Fruit> =  new Array<Fruit>();
 	public var adn:Adn;
-	public function new(_adn:Adn){
+	public var quality:Float;
+	public var nbSeed:Int;
+	
+	public function new (_adn:Adn,_quality:Float = 0) {
         super();
-        adn   = _adn;
+		adn     = _adn;
+		quality = _quality;
+        etablishNbSeed();
+        list.push(this);
+	}
+
+	public function etablishNbSeed () {
+		nbSeed = 3;
+	}
+
+	public function open (knife:String):Array<Seed> {
+		list.splice(list.indexOf(this),1);
+		return new Array<Seed>();
 	}
 }
