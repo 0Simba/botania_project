e3609424b40d65627df3bc21bfeeadc558977cb9
Clement <c.garnier@student.isartdigital.com>
2015-02-18 23:21:23 +0100

    Maj graphique Header

--- src/init/PopUps/HeaderPopUpInit.hx
+++ src/init/PopUps/HeaderPopUpInit.hx
@@ -11,12 +11,14 @@ class HeaderPopUpInit
 
         var headerPopUp:PopUp = popUpEngine.createPopUp("header", new Vector2(0.15, 0), new Vector2(0.7, 0.25, "%", "%x"));
         headerPopUp.addBloc("headerGround", new Vector2(0, 0), new Vector2(1, 0.83));
-        headerPopUp.addButton(new Vector2(0.74, 0.71), new Vector2(0.09, 0.85, "%", "%x"), "chest", "chest", "chest", function () {
+        headerPopUp.addButton(new Vector2(0.74, 0.71), new Vector2(0.09, 0.85, "%", "%x"), "inventoryBtn", "inventoryBtn", "inventoryBtn", function () {
             popUpEngine.show("inventoryInterface");
         });
-        headerPopUp.addButton(new Vector2(0.86, 0.71), new Vector2(0.09, 0.85, "%", "%x"), "caddy", "caddy", "caddy", function () {
+        headerPopUp.addButton(new Vector2(0.86, 0.71), new Vector2(0.09, 0.85, "%", "%x"), "shopBtn", "shopBtn", "shopBtn", function () {
         });
-        var t = headerPopUp.addText(new Vector2(0.02, 0.3), new Vector2(0.6, 0.5), "Botania", {"font" : "80px verdana", "fill" : "orange", "dropShadow" : true, "dropShadowColor" : "white", "stroke" : "green", "strokeThickness" : 4});
-        trace(t);
+        headerPopUp.addButton(new Vector2(0.20, 0.71), new Vector2(0.065, 0.85, "%", "%x"), "unmuteBtn", "unmuteBtn", "unmuteBtn", function () {
+        });
+        headerPopUp.addButton(new Vector2(0.28, 0.71), new Vector2(0.065, 0.85, "%", "%x"), "Fullscreen", "Fullscreen", "Fullscreen", function () {
+        });
     }
 }
