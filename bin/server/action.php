eb94787ece702b5ff64af8822a04f53face1ca75
0Simba <b.dersoir@student.isartdigital.com>
2015-02-20 10:37:51 +0100

    Correctly parsed datas in ajax communications

--- bin/server/action.php
+++ bin/server/action.php
@@ -1,11 +1,12 @@
 
 <?php
     $action = mysqli_real_escape_string($mysqli, htmlspecialchars($_GET["action"]));
+    $datas  = isset($_GET["datas"]) ? urldecode($_GET["datas"]) : "null";
 
     if ($action == "test") {
         echo "coucou les copain";
     }
     else if ($action == "buildBreaker") {
-        echo "Ok on cherche a build un concasseur";
+        echo $datas;
     }
 ?>
--- src/utils/AjaxRequest.hx
+++ src/utils/AjaxRequest.hx
@@ -3,9 +3,10 @@ package utils;
 
 class AjaxRequest
 {
-    static public function exec (type:String, datas:haxe.Json = null, callback = null) {
+    static public function exec (type:String, datas:String = null, callback = null) {
         var url:String = "?action=" + type;
-        if (datas != null) url += "&datas=" + datas;
+        if (datas != null) url += "&datas=" + StringTools.urlEncode(datas);
+
         var response = haxe.Http.requestUrl(url);
 
         trace(response);
