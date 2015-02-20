f2d90f0d39170633a81f404d1a41374af7de8275
0Simba <b.dersoir@student.isartdigital.com>
2015-02-20 10:05:32 +0100

    Re add bdd and set possible config.php

---
+++ bin/server/connection/bdd.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+
+$config = [
+    "host"     => "localhost",
+    "user"     => "root",
+    "password" => "",
+    "database" => "botania",
+];
+
+if (file_exists("server/config.php")) include "server/config.php";
+
+$mysqli = new mysqli($config["host"],
+                     $config["user"],
+                     $config["password"],
+                     $config["database"]);
+
+?>
