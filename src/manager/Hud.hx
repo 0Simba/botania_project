3a8ebab67b159b6605b924e504cecb68a07ef2a9
0Simba <b.dersoir@student.isartdigital.com>
2015-02-26 23:23:32 +0100

    Remove useless hud element

--- src/manager/Hud.hx
+++ src/manager/Hud.hx
@@ -5,8 +5,6 @@ import entities.Tile;
 class Hud
 {
     static public function init () {
-        new entities.biomeHud.Automn();
-        new entities.biomeHud.Grass();
         new entities.flowerHud.Breaker();
         new entities.flowerHud.TestFlower();
     }
