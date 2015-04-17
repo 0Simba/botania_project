161a6a302648cbf865138a6f88325a23f85cf4e8
0Simba <b.dersoir@student.isartdigital.com>
2015-04-17 11:15:44 +0200

    Merge seed animation step 2

--- src/entities/MergeAnimation.hx
+++ src/entities/MergeAnimation.hx
@@ -13,14 +13,18 @@ class MergeAnimation
     private var leftSeedMotif:Hud;
     private var rightSeedColo:Hud;
     private var rightSeedMotif:Hud;
+    private var centerSeedColo:Hud;
+    private var centerSeedMotif:Hud;
     private var overlay:Hud;
     private var tween:Tween;
 
     private function new () {
-        leftSeedColo   = new Hud();
-        rightSeedColo  = new Hud();
-        leftSeedMotif  = new Hud();
-        rightSeedMotif = new Hud();
+        leftSeedColo    = new Hud();
+        rightSeedColo   = new Hud();
+        leftSeedMotif   = new Hud();
+        rightSeedMotif  = new Hud();
+        centerSeedMotif = new Hud();
+        centerSeedColo  = new Hud();
 
         createOverlay();
         createTween();
@@ -40,9 +44,20 @@ class MergeAnimation
 
     static public function anim (codeA:String, codeB:String, codeMerge:String) {
         instance.leftSeedColo.set(  size, leftPosition,  "colo"  + codeA.charAt(0) + codeA.charAt(2), "overlay");
+        instance.leftSeedColo.sprite.visible = true;
         instance.leftSeedMotif.set( size, leftPosition,  "motif" + codeA.charAt(0) + codeA.charAt(1), "overlay");
+        instance.leftSeedMotif.sprite.visible = true;
+
         instance.rightSeedColo.set( size, rightPosition, "colo"  + codeB.charAt(0) + codeB.charAt(2),  "overlay");
+        instance.rightSeedColo.sprite.visible = true;
         instance.rightSeedMotif.set(size, rightPosition, "motif" + codeB.charAt(0) + codeB.charAt(1), "overlay");
+        instance.rightSeedMotif.sprite.visible = true;
+
+        instance.centerSeedColo.set( size, targetPosition, "colo"  + codeMerge.charAt(0) + codeMerge.charAt(2),  "overlay");
+        instance.centerSeedColo.sprite.visible = false;
+        instance.centerSeedMotif.set(size, targetPosition, "motif" + codeMerge.charAt(0) + codeMerge.charAt(1), "overlay");
+        instance.centerSeedMotif.sprite.visible = false;
+
         instance.overlay.sprite.visible = true;
 
         instance.tween.start();
@@ -61,20 +76,19 @@ class MergeAnimation
 
         tween.onUpdate(function (currentDatas) {
             var ratio = currentDatas.get("ratio");
-            var newLeftPosition  = new Vector2 (leftPosition.x + (targetPosition.x - leftPosition.x) * ratio, targetPosition.y);
-            var newRightPosition = new Vector2 (rightPosition.x + (targetPosition.x - rightPosition.x) * ratio, targetPosition.y);
-
-            leftSeedColo.replace(newLeftPosition);
-            leftSeedMotif.replace(newLeftPosition);
-            rightSeedColo.replace(newRightPosition);
-            rightSeedMotif.replace(newRightPosition);
+            replaceSeeds(ratio);
         });
 
         tween.onComplete(function () {
-            leftSeedColo.sprite.visible   = false;
-            leftSeedMotif.sprite.visible  = false;
-            rightSeedColo.sprite.visible  = false;
-            rightSeedMotif.sprite.visible = false;
+            hideSeeds();
+            centerSeedColo.sprite.visible  = true;
+            centerSeedMotif.sprite.visible = true;
+
+            haxe.Timer.delay(function () {
+                centerSeedColo.sprite.visible  = false;
+                centerSeedMotif.sprite.visible = false;
+                overlay.sprite.visible = false;
+            }, 1000);
         });
     }
 
@@ -84,6 +98,25 @@ class MergeAnimation
         overlay.set(Vector2.full, Vector2.zero, "white", "overlay");
         overlay.sprite.alpha   = 0.6;
         overlay.sprite.visible = false;
+    }
+
+
+    private function replaceSeeds (ratio:Float) {
+        var newLeftPosition  = new Vector2 (leftPosition.x + (targetPosition.x - leftPosition.x) * ratio, targetPosition.y);
+        var newRightPosition = new Vector2 (rightPosition.x + (targetPosition.x - rightPosition.x) * ratio, targetPosition.y);
+
+        leftSeedColo.replace(newLeftPosition);
+        leftSeedMotif.replace(newLeftPosition);
+        rightSeedColo.replace(newRightPosition);
+        rightSeedMotif.replace(newRightPosition);
+    }
+
+
+    private function hideSeeds () {
+        leftSeedColo.sprite.visible   = false;
+        leftSeedMotif.sprite.visible  = false;
+        rightSeedColo.sprite.visible  = false;
+        rightSeedMotif.sprite.visible = false;
     }
 }
 
