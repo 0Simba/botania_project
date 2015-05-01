d0fdc63de1312a8836abefb728aed93dab70d649
0Simba <b.dersoir@student.isartdigital.com>
2015-05-01 18:52:20 +0200

    Refactor server folder

--- bin/server/action.php
+++ bin/server/action.php
@@ -7,16 +7,23 @@
 
 
 
-    if      ($action == "buildBuilding")   include "server/buildBuilding.php";
-    else if ($action == "buildFlower")     include "server/buildFlower.php";
-    else if ($action == "flowerGrow")      include "server/flowerGrow.php";
-    else if ($action == "destroyBuilding") include "server/destroyBuilding.php";
-    else if ($action == "destroyFlower")   include "server/destroyFlower.php";
-    else if ($action == "allDatas")        include "server/sendAllDatas.php";
-    else if ($action == "addSeed")         include "server/addSeed.php";
-    else if ($action == "removeSeed")      include "server/removeSeed.php";
-    else if ($action == "addFruit")        include "server/addFruit.php";
-    else if ($action == "removeFruit")     include "server/removeFruit.php";
-    else if ($action == "shopDatas")       include "server/shopDatas.php";
-	else if ($action == "hasEnoughMoney")  include "server/hasEnoughMoney.php";
+    if      ($action == "buildBuilding")   include "server/actions/buildings/buildBuilding.php";
+    else if ($action == "destroyBuilding") include "server/actions/buildings/destroyBuilding.php";
+
+    else if ($action == "addSeed")         include "server/actions/seeds/addSeed.php";
+    else if ($action == "removeSeed")      include "server/actions/seeds/removeSeed.php";
+
+    else if ($action == "addFruit")        include "server/actions/fruits/addFruit.php";
+    else if ($action == "removeFruit")     include "server/actions/fruits/removeFruit.php";
+
+
+    else if ($action == "buildFlower")     include "server/actions/flowers/buildFlower.php";
+    else if ($action == "flowerGrow")      include "server/actions/flowers/flowerGrow.php";
+    else if ($action == "destroyFlower")   include "server/actions/flowers/destroyFlower.php";
+
+    else if ($action == "allDatas")        include "server/actions/sendAllDatas.php";
+    else if ($action == "shopDatas")       include "server/actions/shopDatas.php";
+    else if ($action == "hasEnoughMoney")  include "server/actions/hasEnoughMoney.php";
+
+
 ?>
